c943b837a89a9f4375dd8a802ada9b2d7fc047b4
mulargui <mauricio@ulargui.com>
2014-11-01 14:44:16 -0700

    sync with laptop

---
+++ constants.js
@@ -0,0 +1,9 @@
+var host='127.0.0.1';
+var user='root';
+var password='awsawsdb';
+var database='healthylinkx';
+
+exports.host=host;
+exports.user=user;
+exports.password=password;
+exports.database=database;
